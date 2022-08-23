d3a0e4663194feb684c7795e2c8e51577fc355bb
Daniil4949 <daniilkimstachp@gmail.com>
2022-08-23 19:42:10 +0300

    Before creating logout function

--- tasks/urls.py
+++ tasks/urls.py
@@ -10,5 +10,4 @@ urlpatterns = [
     path("mark_as_completed/<slug:slug>/", mark_as_comleted, name='mark_as_completed'),
     path("delete_task/<slug:slug>/", delete_task, name='delete_task'),
     path("delete_all_tasks/", delete_all_tasks, name='delete_all_tasks'),
-
 ]
