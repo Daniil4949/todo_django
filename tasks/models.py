0ba0965721e8315888e9ac5eb30f73777d4b3df2
Daniil4949 <daniilkimstachp@gmail.com>
2022-08-28 08:19:31 +0300

    Model Task and form for adding tasks were updated

--- tasks/models.py
+++ tasks/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 
 class Task(models.Model):
     title = models.CharField(max_length=255)
-    description = models.TextField(null=True)
+    description = models.TextField(null=True, default='Description is empty')
     is_completed = models.BooleanField(default=False)
     slug = models.SlugField(max_length=1000, db_index=True, null=True, blank=True, verbose_name='URL', unique=False)
     profile = models.ForeignKey('Profile', on_delete=models.CASCADE)
